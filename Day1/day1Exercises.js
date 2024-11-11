0a036cefcc3d4952c3e11facbf67eb22e8e69938
SumeyyeB <sumeyyebalkaya6@gmail.com>
2024-11-11 16:57:27 +0300

    first commit

---
+++ Day1/day1Exercises.js
@@ -0,0 +1,46 @@
+//!   💻 Day 1: Exercises
+// 1. Write a single line comment which says, comments can make code readable
+// Comments can make code readable
+
+// 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/* Comments can make code readable, 
+easy to reuse and informative */
+
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let yourEducation = "FullStack Developer";
+let areYouJobless = true;
+let jobPrice;
+let yourInterview = null;
+
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof yourEducation, areYouJobless, jobPrice, yourInterview);
+
+// 6.  Declare four variables without assigning values
+let x;
+let y;
+let z;
+let w;
+
+// 7.  Declare four variables with assigned values
+let first_Name = "Alice";
+let last_Name = "Smith";
+let marital_Status = "single";
+let country = "USA";
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Sumeyye";
+let lastName = "BALKAYA";
+let maritalStatus = "single";
+let myCountry = "Turkiye";
+let my_Age = 23;
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 23;
+let yourAge = 26;
+console.log("My age is " + myAge);
+console.log("Your age is" + " " + yourAge);
---
+++ Day1/introduction.js
@@ -0,0 +1,27 @@
+console.log(`Bu yazı introduction.js'den geldi!`);
+
+/* Shift+Alt+A yaparak
+block yorum ekleyebilirsin. */
+
+console.log(typeof `Hello`);
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.clear();
+
+// To declare a variable, we use var, let, or const keywords.
+
+let yourName = "Sumeyye";
+let yourAge = 23;
+let are_Still_Single = true;
+let secondName = null;
+let yourMoney = undefined;
+
+console.log(are_Still_Single);
+console.log(secondName);
+console.log(yourName, yourAge, yourMoney);
+
+const PI = 3.14;
+const GRAVITY = 9.18;
+console.log(PI, GRAVITY);
